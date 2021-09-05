9e6400066ae5ad38a45c855ef92abfccdde92bff
Slaven <slaven.cvi@hotmail.com>
2021-09-05 11:51:02 +0200

    map w/h

--- pages/kontakt.vue
+++ pages/kontakt.vue
@@ -3,16 +3,18 @@
     <div class="map">
       <div class="mapouter">
         <div class="gmap_canvas">
-          <iframe
-            width="1400"
-            height="700"
-            id="gmap_canvas"
-            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1714.3591552324826!2d19.690565602253066!3d43.72723558392807!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47582ff55912f42f%3A0xb16d87d45a7670fa!2sRujanska%2C%20Zlatibor%2C%20Serbia!5e0!3m2!1sen!2sus!4v1630780536176!5m2!1sen!2sus"
-            frameborder="0"
-            scrolling="no"
-            marginheight="0"
-            marginwidth="0"
-          ></iframe>
+          <client-only>
+            <iframe
+              :width="mapWidth"
+              :height="mapHeight"
+              id="gmap_canvas"
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1714.3591552324826!2d19.690565602253066!3d43.72723558392807!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47582ff55912f42f%3A0xb16d87d45a7670fa!2sRujanska%2C%20Zlatibor%2C%20Serbia!5e0!3m2!1sen!2sus!4v1630780536176!5m2!1sen!2sus"
+              frameborder="0"
+              scrolling="no"
+              marginheight="0"
+              marginwidth="0"
+            ></iframe>
+          </client-only>
 
           <!-- src="https://maps.google.com/maps?q=zlatibor&t=&z=13&ie=UTF8&iwloc=&output=embed" -->
         </div>
@@ -22,7 +24,42 @@
 </template>
 
 <script>
-export default {}
+export default {
+  computed: {
+    mapWidth() {
+      if (process.browser) {
+        if (window.innerWidth <= 820) {
+          return '300'
+        }
+        if (window.innerWidth <= 1100) {
+          return '700'
+        }
+        if (window.innerWidth <= 1400) {
+          return '1000'
+        }
+
+        return '1400'
+      }
+      return '1400'
+    },
+    mapHeight() {
+      if (process.browser) {
+        if (window.innerWidth <= 820) {
+          return '300'
+        }
+
+        if (window.innerWidth <= 1100) {
+          return '400'
+        }
+        if (window.innerWidth <= 1400) {
+          return '500'
+        }
+        return '700'
+      }
+      return '700'
+    },
+  },
+}
 </script>
 
 <style lang="scss" scoped>
